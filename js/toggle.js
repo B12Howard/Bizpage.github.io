4ad1357f771bfcc2b492d0eff8d9054ca2b69f98
B12Howard <howard.luong87@gmail.com>
2020-10-25 15:18:07 -0700

    toggle to change about div size

--- js/toggle.js
+++ js/toggle.js
@@ -2,13 +2,24 @@ $(function() {
   $(".about-toggle").bind("click", function(event) {
     var $el = $(this);
     var $par = $el.parent();
-    console.log("TEXTfasdf");
+    var $abtContainer = $("#about-container");
+    var abtContainerHeight = $("#about-container").height();
+    console.log("about container");
+    console.log($abtContainer.height());
+    console.log($par.children("div").height());
     if ($par.children("div").is(":hidden")) {
       $par.children("div").removeClass("hidden");
       $el.addClass("fa-plus").removeClass("fa-minus");
+      console.log($par.children("div").height());
+      var h = $par.children("div").height();
+      $abtContainer.height(abtContainerHeight + h);
     } else {
+      var h = $par.children("div").height();
       $par.children("div").addClass("hidden");
       $el.addClass("fa-minus").removeClass("fa-plus");
+      console.log($par.children("div").height());
+
+      $abtContainer.height(abtContainerHeight - h);
     }
   });
 });
