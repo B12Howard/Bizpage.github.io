e82fc11d0566b65837498fbba4fa5b10876468c6
B12Howard <howard.luong87@gmail.com>
2020-11-20 14:33:31 -0800

    style and copy

--- _site/js/toggle.js
+++ _site/js/toggle.js
@@ -1,24 +1,18 @@
-$(function() {
-  $(".about-toggle").bind("click", function(event) {
+$(function () {
+  $(".about-toggle").bind("click", function (event) {
     var $el = $(this);
     var $par = $el.parent();
     var $abtContainer = $("#about-container");
     var abtContainerHeight = $("#about-container").height();
-    console.log("about container");
-    console.log($abtContainer.height());
-    console.log($par.children("div").height());
     if ($par.children("div").is(":hidden")) {
       $par.children("div").removeClass("hidden");
       $el.addClass("fa-plus").removeClass("fa-minus");
-      console.log($par.children("div").height());
-      var h = $par.children("div").height();
+      var h = $par.children("div").height() * 1.2;
       $abtContainer.height(abtContainerHeight + h);
     } else {
-      var h = $par.children("div").height();
+      var h = $par.children("div").height() * 1.2;
       $par.children("div").addClass("hidden");
       $el.addClass("fa-minus").removeClass("fa-plus");
-      console.log($par.children("div").height());
-
       $abtContainer.height(abtContainerHeight - h);
     }
   });
